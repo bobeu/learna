14c1c493117071b4c5aef3dde7844e802416efd2
bobeu <bobmatea27@gmail.com>
2025-07-12 08:14:41 +0100

    Removed blocking metadata

--- eduFi/src/app/layout.tsx
+++ eduFi/src/app/layout.tsx
@@ -8,20 +8,20 @@ import { APP_NAME, APP_DESCRIPTION } from "~/lib/constants";
 export const metadata: Metadata = {
   title: APP_NAME,
   description: APP_DESCRIPTION,
-  openGraph: {
-    title: `${APP_NAME} - Challenge Your Knowledge`,
-    description: "Test your knowledge with engaging quizzes across various topics",
-    images: ["https://images.pexels.com/photos/301920/pexels-photo-301920.jpeg?auto=compress&cs=tinysrgb&w=1200"],
-    type: "website"
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "Challenge Your Knowledge",
-    description: "Test your knowledge with engaging quizzes across various topics",
-    images: "https://images.pexels.com/photos/301920/pexels-photo-301920.jpeg?auto=compress&cs=tinysrgb&w=1200",
-    creator: "https://.com/bobman7000",
-    site: "https://learna.vercel.app"
-  }
+  // openGraph: {
+  //   title: `${APP_NAME} - Challenge Your Knowledge`,
+  //   description: "Test your knowledge with engaging quizzes across various topics",
+  //   images: ["https://images.pexels.com/photos/301920/pexels-photo-301920.jpeg?auto=compress&cs=tinysrgb&w=1200"],
+  //   type: "website"
+  // },
+  // twitter: {
+  //   card: "summary_large_image",
+  //   title: "Challenge Your Knowledge",
+  //   description: "Test your knowledge with engaging quizzes across various topics",
+  //   images: "https://images.pexels.com/photos/301920/pexels-photo-301920.jpeg?auto=compress&cs=tinysrgb&w=1200",
+  //   creator: "https://.com/bobman7000",
+  //   site: "https://learna.vercel.app"
+  // }
 };
 
 export default async function RootLayout({
