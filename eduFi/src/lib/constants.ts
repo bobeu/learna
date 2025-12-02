012bcec430a03f9b4f361ff4524c004a7b711686
bobeu <bobmatea27@gmail.com>
2025-12-02 12:39:52 +0100

    Edited lib

--- eduFi/src/lib/constants.ts
+++ eduFi/src/lib/constants.ts
@@ -3,7 +3,7 @@ import { Address } from "../../types";
 
 export const RECEIVER = "0xa1f70ffA4322E3609dD905b41f17Bf3913366bC1" as Address;
 export const APP_URL = process.env.NEXT_PUBLIC_URL!;
-export const APP_NAME = `${process.env.NEXT_PUBLIC_MINI_APP_NAME} - Challenge Your Knowledge`;
+export const APP_NAME = process.env.NEXT_PUBLIC_MINI_APP_NAME as string;
 export const APP_DESCRIPTION = process.env.NEXT_PUBLIC_MINI_APP_DESCRIPTION;
 export const APP_PRIMARY_CATEGORY = process.env.NEXT_PUBLIC_MINI_APP_PRIMARY_CATEGORY;
 export const APP_TAGS = process.env.NEXT_PUBLIC_MINI_APP_TAGS?.split(',');
